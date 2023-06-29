79044f4268c8990b93d5bd636cf476091e5320e4
Yasser Loukniti <yasser.loukniti@estiam.com>
2023-06-29 16:45:39 +0200

    Insert Fake Data

--- cmd/index/index.go
+++ cmd/index/index.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"os"
 	"time"
+
+	"github.com/YasserLoukniti/crowlerGo/pkg/protocols"
 )
 
 const (
@@ -15,7 +17,25 @@ const (
 )
 
 func main() {
+	database := protocols.Database{}
+	database.Sites = append(database.Sites, protocols.Site{
+		Id:     1,
+		HostIp: "212.27.63.171",
+		Domain: "http://maxicool5.free.fr/Potager/",
+	})
+	database.Files = append(database.Files, protocols.File{
+		Id:     1,
+		Name:   "Calendrier semis 1.jpg",
+		Url:    "http://maxicool5.free.fr/Potager/Calendrier%20semis%201.jpg",
+		SiteId: 1,
+	}, protocols.File{
+		Id:     2,
+		Name:   "Calendrier semis 2.jpg",
+		Url:    "http://maxicool5.free.fr/Potager/Calendrier%20semis%202.jpg",
+		SiteId: 1,
+	})
 	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
@@ -30,8 +50,8 @@ func main() {
 		}
 		go handleRequest(conn)
 	}
-}
 
+}
 func handleRequest(conn net.Conn) {
 	// incoming request
 	buffer := make([]byte, 1024)
