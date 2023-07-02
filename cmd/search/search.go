de99213ad92993208f412e12b62a0dfa3297d52a
YL <yass_official@outlook.fr>
2023-07-02 21:38:02 +0200

    fix search

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"os"
 
 	"github.com/YasserLoukniti/crowlerGo/pkg/protocols"
 	"github.com/gorilla/mux"
@@ -21,47 +20,17 @@ const (
 )
 
 func main() {
-	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
-
-	if err != nil {
-		println("ResolveTCPAddr failed:", err.Error())
-		os.Exit(1)
-	}
-
-	conn, err := net.DialTCP(TYPE, nil, tcpServer)
-	if err != nil {
-		println("Dial failed:", err.Error())
-		os.Exit(1)
-	}
-
-	_, err = conn.Write([]byte("This is a message"))
-	if err != nil {
-		println("Write data failed:", err.Error())
-		os.Exit(1)
-	}
-
-	// buffer to get data
-	received := make([]byte, 1024)
-	_, err = conn.Read(received)
-	if err != nil {
-		println("Read data failed:", err.Error())
-		os.Exit(1)
-	}
-
-	println("Received message:", string(received))
-
-	conn.Close()
 
 	// gorilla/mux => HTTP
 	router := mux.NewRouter()
-	router.HandleFunc("/search", createUrlHandler).Methods("POST")
+	router.HandleFunc("/search", createSiteHandler).Methods("POST")
 
 	// Start The Server
 	fmt.Printf("Serveur HTTP démarré sur http://%s:%s\n", HTTP_ADRESS, HTTP_PORT)
 	log.Fatal(http.ListenAndServe(HTTP_ADRESS+":"+HTTP_PORT, router))
 }
 
-func createUrlHandler(resWriter http.ResponseWriter, req *http.Request) {
+func createSiteHandler(resWriter http.ResponseWriter, req *http.Request) {
 	// Lire le corps (body) de la requête
 	var requestData struct {
 		URL string `json:"url"`
@@ -72,7 +41,7 @@ func createUrlHandler(resWriter http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial(TYPE, HOST+":"+PORT)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -82,10 +51,9 @@ func createUrlHandler(resWriter http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 	}
 
-	request := protocols.GenericRequest{
-		Command: "createFile",
-	}
-
+	request := protocols.CreateOrUpdateSiteRequest{}
+	request.Command = "createSite"
+	request.Site = protocols.Site{Domain: requestData.URL}
 	requestBuffer, err := json.Marshal(request)
 	if err != nil {
 		log.Fatal(err)
@@ -107,15 +75,4 @@ func createUrlHandler(resWriter http.ResponseWriter, req *http.Request) {
 
 	// Écrire la réponse JSON dans le corps de la réponse
 	resWriter.Write([]byte("ok"))
-
-}
-
-func getIndex() []string {
-	// Retourner ici le tableau d'URLs de l'index
-	index := []string{
-		"http://example.com/page1",
-		"http://example.com/page2",
-		"http://example.com/page3",
-	}
-	return index
 }
